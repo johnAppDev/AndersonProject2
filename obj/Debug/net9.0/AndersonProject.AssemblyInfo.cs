817d9d45a15d68510b05d75798e52632a117a3dc
johnAppDev <99500965+johnAppDev@users.noreply.github.com>
2025-04-14 14:01:52 -0500

    Add pins

--- obj/Debug/net9.0/AndersonProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/AndersonProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AndersonProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca2cf78f1f93ba97b53d8698e012a79b86f9f08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1341e7a00049160d3443c1a47ee240450974f093")]
 [assembly: System.Reflection.AssemblyProductAttribute("AndersonProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AndersonProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/map.js
+++ wwwroot/map.js
@@ -1,12 +1,21 @@
 var map
+var markersGroup
 function createMap(lat, long, zoom){
      map = L.map('map').setView([lat, long], zoom);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
+        maxZoom: 30,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-}
-function addPin(lat, long){
-    var marker = L.marker([lat, long]).addTo(map);
-    marker.bindPopup("<img src=\"https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHM2NXl2M3Q3YXNheTh6aHFudGk4NHNoaHZybmFvMHVvOGcxOTlveSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/nDSlfqf0gn5g4/giphy.gif\" alt=\"Computer man\" style=\"width:48px;height:48px;\"><br>This is how readers and panels will be displayed.").openPopup();
+    markersGroup = L.layerGroup();
+    map.addLayer(markersGroup);
+    map.on('click', function(e){
+        L.circle( e.latlng, {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 5
+        }).addTo(markersGroup);
+        return;
+    });
+
 }
